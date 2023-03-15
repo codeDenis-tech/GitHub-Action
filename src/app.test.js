ffbb6c5c5df1bc1991d204023710c0d0577f4d85
codeDenis-tech <67652452+codeDenis-tech@users.noreply.github.com>
2023-03-14 23:56:27 -0400

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -3,9 +3,9 @@ const supertest = require('supertest');
 const request = supertest(app);
 
 describe('/testNod endpoint', () => {
-	  i('shouldreturn a response', async () >{
-		      const sponse = await reest.get('/testNode');
-		      expect(response.status).toBe(200);
-		      expect(response.text).toBe('Yes the testNode endpont wokdt = require');
-		    });
+  it('should return a response', async () => {
+    const response = await request.get('/testNode');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Yes the testNode endpoint worked');
+  });
 });
